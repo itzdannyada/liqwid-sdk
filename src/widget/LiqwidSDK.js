69394d50688eee12089c3980e92b94619f163c6b
$itzdanny <itzdannyada@gmail.com>
2025-11-10 14:06:27 +0000

    chore: update package versions to 1.1.4 and 1.1.5; import React in LiqwidSDK component

--- src/widget/LiqwidSDK.js
+++ src/widget/LiqwidSDK.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
 import {BsInfoSquareFill} from 'react-icons/bs'
 import './LiqwidSDK.css';
